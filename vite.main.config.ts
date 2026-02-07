ae82384fdcb9ad6d86cb8e1d31813aa982dd9d81
SampoAho <fsaah003@edu.xamk.fi>
2026-02-07 17:33:48 +0200

    Should work now x2

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,7 +4,14 @@ import { builtinModules } from "module";
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["electron", ...builtinModules],
+      external: [
+        "electron",
+        ...builtinModules,
+        "webtorrent",
+        "utp-native",
+        /^node-datachannel/,
+        "@achingbrain/nat-port-mapper",
+      ],
       output: {
         entryFileNames: "[name].cjs",
       },

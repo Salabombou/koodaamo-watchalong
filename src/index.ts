39fa5cdfe5bfe156f04457bbdb24177bad932086
SampoAho <fsaah003@edu.xamk.fi>
2026-02-12 17:25:55 +0200

    Fixed auto update implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { app, BrowserWindow, ipcMain, protocol } from "electron";
-import { autoUpdater } from "electron-updater";
+import { updateElectronApp, UpdateSourceType } from "update-electron-app";
 import logger from "./utilities/logging";
 
 import { StorageService } from "./services/StorageService";
@@ -21,6 +21,8 @@ protocol.registerSchemesAsPrivileged([
   },
 ]);
 
+logger.log("Running app version ", app.getVersion());
+
 // Initialize Services
 let storageService: StorageService;
 logger.info("Initializing services...");
@@ -28,25 +30,33 @@ const mediaService = new MediaService();
 const torrentService = new TorrentService();
 logger.info("Services initialized.");
 
-// Update Configuration
-autoUpdater.logger = logger;
-autoUpdater.autoDownload = true;
-
-// Fix for ENOENT: no such file or directory, open '...resources\app-update.yml'
-// Electron Forge does not generate this file, so we configure it programmatically.
-autoUpdater.setFeedURL({
-  provider: "github",
-  owner: "Salabombou",
-  repo: "koodaamo-watchalong",
+// Enable auto-updates
+updateElectronApp({
+  updateSource: {
+    type: UpdateSourceType.StaticStorage,
+    baseUrl: `https://github.com/Salabombou/koodaamo-watchalong/releases/download/${app.getVersion()}/koodaamo-watchalong-${app.getVersion()}.Setup.exe`
+  },
+  logger: {
+    info: (msg) => logger.info(msg),
+    warn: (msg) => logger.warn(msg),
+    error: (msg) => logger.error(msg),
+    log: (msg) => logger.info(msg),
+  },
 });
 
 // Global Error Handlers
 process.on("uncaughtException", (error) => {
   logger.error("Uncaught Exception:", error);
+  if (error instanceof Error) {
+    logger.error("Stack Trace:", error.stack);
+  }
 });
 
 process.on("unhandledRejection", (reason) => {
   logger.error("Unhandled Rejection:", reason);
+  if (reason instanceof Error) {
+    logger.error("Stack Trace:", reason.stack);
+  }
 });
 
 ipcMain.handle("get-node-version", () => {
@@ -133,7 +143,6 @@ app.on("ready", () => {
   // Check for updates
   try {
     logger.info("Checking for updates...");
-    autoUpdater.checkForUpdatesAndNotify();
   } catch (err) {
     logger.error("Error checking for updates:", err);
   }
@@ -219,35 +228,6 @@ ipcMain.on("torrent:broadcast", (_, cmd) => {
 
 ipcMain.handle("open-player-window", () => {
   createPlayerWindow();
-});
-
-ipcMain.handle("update:restart", () => {
-  autoUpdater.quitAndInstall();
-});
-
-// AutoUpdater events
-autoUpdater.on("update-available", () => {
-  BrowserWindow.getAllWindows().forEach((w) =>
-    w.webContents.send("update:available"),
-  );
-});
-
-autoUpdater.on("download-progress", (progressObj) => {
-  BrowserWindow.getAllWindows().forEach((w) =>
-    w.webContents.send("update:progress", progressObj),
-  );
-});
-
-autoUpdater.on("update-downloaded", () => {
-  BrowserWindow.getAllWindows().forEach((w) =>
-    w.webContents.send("update:downloaded"),
-  );
-});
-
-autoUpdater.on("error", (err) => {
-  BrowserWindow.getAllWindows().forEach((w) =>
-    w.webContents.send("update:error", err.message || err.toString()),
-  );
 });
 
 // Events
